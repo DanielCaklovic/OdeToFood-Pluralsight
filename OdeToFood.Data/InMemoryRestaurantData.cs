cd88613974a5a0f8a906898fbc13eb05c4a428c2
Daniel Čaklović <daniel.caklovic@gmail.com>
2020-10-10 11:34:25 +0200

    initial commit

---
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using OdeToFood.Core;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant() { Id = 1, Name = "Slavonija", Location = "Dakovo", Cuisine = CuisineType.None },
+                new Restaurant() { Id = 2, Name = "Bistro", Location = "Vinkovci", Cuisine = CuisineType.Italian },
+                new Restaurant() { Id = 3, Name = "Lipovac", Location = "Lipovac", Cuisine = CuisineType.Indian }
+            };
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
+        public Restaurant Update(Restaurant updateRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updateRestaurant.Id);
+            if (restaurant != null)
+            {
+                restaurant.Name = updateRestaurant.Name;
+                restaurant.Location = updateRestaurant.Location;
+                restaurant.Cuisine = updateRestaurant.Cuisine;
+            }
+
+            return restaurant;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+
+        public Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+            return from r in restaurants
+                   where string.IsNullOrEmpty(name) || r.Name.ToLower().StartsWith(name.ToLower())
+                   orderby r.Name
+                   select r;
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = restaurants.FirstOrDefault(r => r.Id == id);
+            if(restaurant != null)
+            {
+                restaurants.Remove(restaurant);
+            }
+            return restaurant;
+        }
+
+        public int GetCountOfRestaurants()
+        {
+            return restaurants.Count();
+        }
+    }
+}
---
+++ OdeToFood.Data/OdeToFoodDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }  
+}
